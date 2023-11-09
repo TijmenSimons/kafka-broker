150faadc95fa577fb53d8cd0321dbf6e48561117
TijmenSimons <145433363+VoxturTijmen@users.noreply.github.com>
2023-11-09 09:53:49 +0100

    auto create broker_config.ini

--- src/kafka_broker/config.py
+++ src/kafka_broker/config.py
@@ -1,9 +1,11 @@
 from configparser import ConfigParser
+import os
+from pathlib import Path
 
+current_dir = Path(__file__).parent.resolve()
 
-config_parser = ConfigParser()
-config_parser.read("broker/base_config.ini")
-base_config = dict(config_parser._sections)
+BASE_CONFIG = str(Path(current_dir, "base_config.ini"))
+print(BASE_CONFIG)
 
 
 def check_config(config: dict) -> dict:
@@ -18,3 +20,23 @@ def check_config(config: dict) -> dict:
         raise AttributeError('"general:current_location" needs to be defined')
     
     return config
+
+
+def get_config(filename: str):
+    base_config_parser = ConfigParser()
+    base_config_parser.read(BASE_CONFIG)
+    base_config = dict(base_config_parser._sections)
+    
+    if not os.path.isfile(filename):
+        with open(BASE_CONFIG, 'r') as f:
+            new_config = f.read()
+
+        with open(filename, 'w') as f:
+            f.write(new_config)
+
+    config_parser = ConfigParser()
+    config_parser.read(filename)
+    custom_config = dict(config_parser._sections)
+
+    base_config.update(custom_config)
+    return check_config(base_config)
